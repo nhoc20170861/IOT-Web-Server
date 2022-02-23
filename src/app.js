cb525aacc26d458af099ba392a3aad87f37f6f7e
nhoc20170861 <xinloimoinguoi1999@gmail.com>
2022-02-23 21:29:40 +0700

    update project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-const mqtt = require("mqtt");
-const dotenv = require("dotenv");
-dotenv.config({ path: "src/.env" });
-const express = require('express');
-const { create } = require("express-handlebars");
+const mqtt = require('mqtt');
+const dotenv = require('dotenv');
 const path = require('path');
+dotenv.config({ path: path.join(__dirname, '.env') });
+const express = require('express');
+const { create } = require('express-handlebars');
+const { header } = require('express/lib/request');
 
 const app = express();
 const port_server = process.env.PORT_SERVER;
@@ -16,7 +17,7 @@ const hbs = create({
     layoutsDir: `${__dirname}/resources/views/layouts`,
     extname: `hbs`,
     defaultLayout: 'main',
-    partialsDir: `${__dirname}/resources/views/partials`
+    partialsDir: `${__dirname}/resources/views/partials`,
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -33,42 +34,49 @@ const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
     connectTimeout: 4000,
-    username: "nhoc20170861",
-    password: "nhoc20170861",
+    username: 'nhoc20170861',
+    password: 'nhoc20170861',
     reconnectPeriod: 1000,
 });
 console.log(connectUrl);
 // subscribe
-const topic = "nhoc20170861/mqtt";
-client.on("connect", () => {
-    console.log("Connected");
+const topic = 'nhoc20170861/mqtt';
+client.on('connect', () => {
+    console.log('Connected');
     client.subscribe([topic], () => {
         console.log(`Subscribe to topic '${topic}'`);
     });
 });
 
 // console.log message received
-client.on("message", (topic, payload) => {
-    console.log("Received Message:", topic, payload.toString());
+client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString());
 });
 
-client.on("connect", () => {
+client.on('connect', () => {
     client.publish(
         topic,
-        "nodejs mqtt test",
+        'nodejs mqtt test',
         { qos: 0, retain: false },
         (error) => {
             if (error) {
                 console.error(error);
             }
-        }
+        },
     );
 });
 // Function to listen on the port
 app.listen(port_server, () => {
     console.log(`App listening at http://localhost:${port_server}`);
 });
-// local
+// [Get] localhost:port/
 app.get('/', (req, res) => {
-    res.render("home");
-  })
+    res.render('home');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard',{
+        change_header: true,
+    });
+});
+
--- src/public/index.js
+++ src/public/index.js
@@ -3,25 +3,23 @@ function next(totalPage) {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const value = parseInt(params.page) + 1;
-    if (value > totalPage ) value = totalPage;
-    urlSearchParams.set("page", value);
+    if (value > totalPage) value = totalPage;
+    urlSearchParams.set('page', value);
     window.location.search = urlSearchParams.toString();
-
 }
 function back() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const value = parseInt(params.page) - 1;
     if (value < 1) value = 1;
-    urlSearchParams.set("page", value);
+    urlSearchParams.set('page', value);
     window.location.search = urlSearchParams.toString();
 }
-
 
 // click button start to show quiz at ?page=1
 function showquiz() {
     const data = new URLSearchParams();
-    data.append("page", "1");
-    const url = "/dashboards/showquiz?" + data.toString();
+    data.append('page', '1');
+    const url = '/dashboards/showquiz?' + data.toString();
     location.href = url;
 }
